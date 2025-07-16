d8e795dc44a7d603a7cdd7eb73c36621ee67a10d
NotN465 <eceljaK@gmail.com>
2025-07-16 18:12:34 +0200

    Fixed the bug with right and left button in the my-wishes menu, made a new command 'edit-a-wish' which basically edits a wish based on an ID and 3 optional parameters (title, content and checkmark)

--- main.py
+++ main.py
@@ -73,18 +73,35 @@ class PageView(discord.ui.View):
         self.page = 5
         begin = 0
         end = 0
+        self.wish_buttons = []
+        self.user_wishes = session.query(WishLists).filter_by(user_id=user_id).all()
         self.wish_check_mark_dict = {True: ":white_check_mark:", False: ":x:"}
         self.wish_check_mark_style = {True: discord.ButtonStyle.success,
                                       False: discord.ButtonStyle.danger}
-        self.wish_buttons = []
-        self.user_wishes = session.query(WishLists).filter_by(user_id=user_id).all()
+
+        self.right_button_reference = discord.ui.Button(label="▶️",style=discord.ButtonStyle.secondary)
+        self.right_button_reference.callback = self.right_button
+
+        self.left_button_reference = discord.ui.Button(label="◀️",style=discord.ButtonStyle.secondary)
+        self.left_button_reference.callback = self.left_button
+
+        self.add_item(self.left_button_reference)
+        self.add_item(self.right_button_reference)
+        # This if statements are used for disabling the buttons
+        if self.page == 5:
+            self.left_button_reference.disabled = True
+        else:
+            self.left_button_reference.disabled = False
+
+        if self.page >= len(self.user_wishes):
+            self.right_button_reference.disabled = True
+
         if len(self.user_wishes) > 5:
             end = 5
         else:
             end = len(self.user_wishes)
         self.create_buttons(user_wishes=self.user_wishes[begin:end])
-    @discord.ui.button(label="◀️",style=discord.ButtonStyle.secondary)
-    async def left_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+    async def left_button(self,interaction:discord.Interaction):
         embed = discord.Embed(
             title=f"{interaction.user.name}'s wishes",
             color=discord.Color.brand_green()
@@ -93,26 +110,20 @@ class PageView(discord.ui.View):
         self.user_wishes = session.query(WishLists).filter_by(user_id=user_id).all()
         #print(f'page: {self.page}')
 
-        if self.page <= 5:
-            #button.disabled = True
-            print("Disabling the button.")
-
+        self.page -= 5
+        self.left_button_reference.disabled = False
+        if len(self.user_wishes) < 5:
+            user_wishes = self.user_wishes[0:len(self.user_wishes)]
         else:
-            self.page -= 5
-            button.disabled = False
-            if len(self.user_wishes) < 5:
-                self.user_wishes = self.user_wishes[0:len(self.user_wishes)]
-            else:
-                print(f"user_wishes[{self.page-5}:{self.page}]")
-                self.user_wishes = self.user_wishes[self.page-5:self.page]
-            for wish in self.user_wishes:
-                check_mark = self.wish_check_mark_dict[wish.check_mark]
-                embed.add_field(name=f"ID: {wish.id} {wish.title} - {check_mark}",value=wish.description,inline=False)
-            self.create_buttons(user_wishes=self.user_wishes)
-            await interaction.response.edit_message(embed=embed,view=self)
+            print(f"user_wishes[{self.page-5}:{self.page}]")
+            user_wishes = self.user_wishes[self.page-5:self.page]
+        for wish in user_wishes:
+            check_mark = self.wish_check_mark_dict[wish.check_mark]
+            embed.add_field(name=f"ID: {wish.id} {wish.title} - {check_mark}",value=wish.description,inline=False)
+        self.create_buttons(user_wishes=user_wishes)
+        await interaction.response.edit_message(embed=embed,view=self)
 
-    @discord.ui.button(label="▶️",style=discord.ButtonStyle.secondary)
-    async def right_button(self,interaction:discord.Interaction,button:discord.ui.Button):
+    async def right_button(self,interaction:discord.Interaction):
         embed = discord.Embed(
             title=f"{interaction.user.name}'s wishes",
             color=discord.Color.brand_green()
@@ -121,25 +132,20 @@ class PageView(discord.ui.View):
         self.user_wishes = session.query(WishLists).filter_by(user_id=user_id).all()
         #print(f'page: {self.page}')
 
-        if len(self.user_wishes) <= self.page:
-            #button.disabled = True
-            print("Disabling the button.")
-
+        self.page += 5
+        #print(len(user_wishes),self.page)
+        if len(self.user_wishes) < self.page:
+            user_wishes = self.user_wishes[self.page-5:len(self.user_wishes)]
         else:
-            self.page += 5
-            button.disabled = False
-            #print(len(user_wishes),self.page)
-            if len(self.user_wishes) < self.page:
-                self.user_wishes = self.user_wishes[self.page-5:len(self.user_wishes)]
-            else:
-                self.user_wishes = self.user_wishes[self.page-5:self.page]
-            for wish in self.user_wishes:
-                check_mark = self.wish_check_mark_dict[wish.check_mark]
-                embed.add_field(name=f"ID: {wish.id} {wish.title} - {check_mark}",value=wish.description,inline=False)
-            print(f"wishes: {self.user_wishes}")
-            self.create_buttons(user_wishes=self.user_wishes)
-            await interaction.response.edit_message(embed=embed,view=self)
+            user_wishes = self.user_wishes[self.page-5:self.page]
+        for wish in user_wishes:
+            check_mark = self.wish_check_mark_dict[wish.check_mark]
+            embed.add_field(name=f"ID: {wish.id} {wish.title} - {check_mark}",value=wish.description,inline=False)
+        #print(f"wishes: {user_wishes}")
+        self.create_buttons(user_wishes=user_wishes)
+        await interaction.response.edit_message(embed=embed,view=self)
     async def update(self,user_wishes,interaction,view):
+
         embed = discord.Embed(
             title=f"{interaction.user.name}'s wishes",
             color=discord.Color.brand_green()
@@ -149,6 +155,19 @@ class PageView(discord.ui.View):
             embed.add_field(name=f"ID: {wish.id} {wish.title} - {check_mark}", value=wish.description, inline=False)
         await interaction.response.edit_message(embed=embed,view=view)
     def create_buttons(self,user_wishes):
+        # For disabling the buttons (the function is not properly named
+        # for this, but I didn't want to change its name now)
+        if self.page == 5:
+            self.left_button_reference.disabled = True
+        else:
+            self.left_button_reference.disabled = False
+        if self.page >= len(self.user_wishes):
+            #print(f"self.page: {self.page} - self.user_wishes: {self.user_wishes}\nDisabling the right button.")
+            self.right_button_reference.disabled = True
+        else:
+            print("Enabling the right button.")
+            self.right_button_reference.disabled = False
+
         if self.wish_buttons != []:
             self.remove_wish_buttons()
         check_mark_dict = {True: "✔️",False:"❌"}
@@ -236,5 +255,31 @@ async def delete_wish(interaction:discord.Interaction,id: str):
     session.delete(wish_to_delete)
     session.commit()
     await interaction.response.send_message(f"You successfully deleted a wish with the ID: {id}")
+@bot.tree.command(name="edit-a-wish",description="edits an already made wish.")
+@app_commands.describe(id="ID",title="title",content="content",checkmark="check mark")
+async def edit_a_wish(interaction:discord.Interaction,id: str, title: str = "optional", content: str = "optional", checkmark: bool = False):
+    user_id = str(interaction.user.id)
+    wish = session.query(WishLists).filter(WishLists.id == id).first()
+    if wish.user_id != user_id:
+        await interaction.response.send_message("You can't edit a wish that isn't yours.")
+        return
+    if title == 'optional':
+        title = wish.title
+    if content == 'optional':
+        content = wish.description
+    wish.title = title
+    wish.description = content
+    wish.check_mark = checkmark
+    session.commit()
+
+    check_mark_dict = {True: "✔️", False: "❌"}
+
+    embed = discord.Embed(
+        title=f"Editing a wish with the ID {wish.id} for {interaction.user.name}",
+        color=discord.Color.brand_green()
+    )
+    embed.add_field(name=title + f" - {check_mark_dict[wish.check_mark]}",value=content,inline=False)
+    view = AddWishView(wish_reference=wish,content=content)
+    await interaction.response.send_message(embed=embed,view=view)
 
 bot.run(token)
